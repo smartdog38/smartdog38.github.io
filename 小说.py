dc755e011fb95ff8ca1a5e7357e3952d9c33036a
smart dogs <3476054544@qq.com>
2024-06-23 14:47:52 +0800

    爬取纵横小说网

--- 小说.py
+++ 小说.py
@@ -1,25 +1,44 @@
+import time
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import  json
 
-url = 'https://fanqienovel.com/page/7264473116506590243?enter_from=stack-room'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0',
 }
-page = requests.get(url=url,headers=headers).text
-# print(page)
-soup = BeautifulSoup(page,'html.parser')
-chapter_list = soup.find_all("a",attrs={"class":"chapter-item-title"})
-# print(chapter_list)
-for chapter in chapter_list:
-    href = chapter.get("href")
-    chapter_url = urljoin(url,href)
-    print(chapter_url)
-    chapter_data = requests.get(url=chapter_url,headers=headers).text
-    # print(chapter_data)
 
-    chapter_soup = BeautifulSoup(chapter_data,'html.parser')
-    word_list = chapter_soup.find_all("div",attrs={"class":"muye-reader-content noselect"})
-    print(word_list)
-    break
+#爬取纵横小说网
+chapter_url = 'https://bookapi.zongheng.com/api/chapter/getChapterList'
+data = {
+'bookId': '1322577'#每个书的ID不一样
+}
+chapter_page = requests.post(url = chapter_url,headers=headers,data=data).json()
+# print(chapter_page)
+# soup_chapter = BeautifulSoup(chapter_url,'html.parser')
 
+chapter_ID_list = chapter_page['result']['chapterList'][0]['chapterViewList']
+# print(chapter_ID_list)
+with open("让你下山娶妻，不是让你震惊世界.txt","w",encoding="utf-8") as f:
+    for chapter_ID in chapter_ID_list:
+        content = ''
+        chapterId = chapter_ID['chapterId']
+        chapter_url_f = f'https://read.zongheng.com/chapter/1322577/{chapterId}.html'
+        # print(chapter_url_f)
+        page = requests.get(url=chapter_url_f, headers=headers).text
+        # print(page)
+        soup = BeautifulSoup(page, 'html.parser')
+        # 找到网页的小说内容
+        title = soup.find('div', attrs={"class": "title_txtbox"}).get_text()
+        print(title)
+        f.write(title)
+        f.write("\n")
+        p_list = soup.find_all('div', attrs={'class': 'content'})
+        for p in p_list:
+            content += p.get_text()
+            # print(content)
+        f.write(content)
+        f.write("\n")
+        print(content)
+        time.sleep(1)
+    f.close()
